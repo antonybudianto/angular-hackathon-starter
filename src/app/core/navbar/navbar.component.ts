4f0e49bdf58a07f98f09918aac4b77bcd7f9e61c
Antony <antony.budianto@gdplabs.id>
2016-11-30 12:33:25 +0700

    fix navbar button style

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -3,6 +3,9 @@ import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 @Component({
     selector: 'app-navbar',
     templateUrl: 'navbar.component.html',
+    styleUrls: [
+        './navbar.component.css'
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
