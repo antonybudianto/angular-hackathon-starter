ab5b4182687ae4711f1b16e45caabdaa277f13b1
Antony Budianto <antonybudianto@gmail.com>
2017-01-02 09:44:49 +0700

    refactor firebase config

--- src/app/core/firebase/config.ts
+++ src/app/core/firebase/config.ts
@@ -1,7 +1,8 @@
-let firebaseConfig: any = {};
-firebaseConfig['apiKey'] =  process.env.FIREBASE_API_KEY;
-firebaseConfig['authDomain'] = process.env.FIREBASE_AUTH_DOMAIN;
-firebaseConfig['databaseURL'] = process.env.FIREBASE_DATABASE_URL;
-firebaseConfig['storageBucket'] = process.env.FIREBASE_STORAGE_BUCKET;
+const apiKey: string =  process.env.FIREBASE_API_KEY;
+const authDomain: string = process.env.FIREBASE_AUTH_DOMAIN;
+const databaseURL: string = process.env.FIREBASE_DATABASE_URL;
+const storageBucket: string = process.env.FIREBASE_STORAGE_BUCKET;
 
-export {firebaseConfig};
+export const firebaseConfig = {
+    apiKey, authDomain, databaseURL, storageBucket
+};
