31fbd1ab076f716f215c118917de253efd65b0c8
Antony Budianto <antonybudianto@gmail.com>
2017-02-26 19:05:20 +0700

    Merge pull request #127 from antonybudianto/update-dep
    updatedep

--- src/app/guest/reset-password/reset-password.component.ts
+++ src/app/guest/reset-password/reset-password.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
     ]
 })
 export class ResetPasswordComponent {
-    submitted: boolean = false;
+    submitted = false;
     errorMessage: string;
     isLoading: boolean;
     email: string;
