8835c82f32c82a240d5216dbcb602fdccec8a31e
hendryfoe <hendryanto.fudiko@gmail.com>
2017-04-20 21:05:40 +0700

    move global import rxjs to main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
+import './rx';
 import { AppModule } from './app/app.module';
 
 if (process.env.APP_ENV === 'production') {
---
+++ src/rx.ts
@@ -0,0 +1,6 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
